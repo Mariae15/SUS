37bdfeae0777b61e831038018e013cedb4505ef3
Maria <maria.patricio.silva@escola.pr.gov.br>
2025-03-18 19:16:14 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,7 @@
+function apareceFrase() {
+document.getElementById('texto').style.display = 'block'
+}
+
+function desapareceFrase() {
+    document.getElementById('texto').style.display = 'none'
+}
